3988e458a1acea7cb2d90616c3ba0219c8cc2e6e
MohamedYara <yara46403@gmail.com>
2024-12-27 20:56:37 +0100

    Le Layout content le master

---
+++ smart-pump/resources/views/Layouts/master.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+<meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="shortcut icon" type="x-icon" href="img/icons/Icon title.png" style="width: 100%; height: 400px;">
+    <title>Smart pump- Tableau de bord</title>
+
+    <!-- Custom fonts for this template-->
+    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.1/css/all.min.css" integrity="sha512-5Hs3dF2AEPkpNAR7UiOHba+lRSJNeM2ECkwxUIxC1Q/FLycGTbNapWXB4tP889k5T5Ju8fs4b1P5z/iB4nMfSQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link
+        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
+        rel="stylesheet">
+        <link rel="stylesheet" href="{{asset('css.sb-admin-2.min.css')}}">
+</head>
+<body>
+   @include('component.navbar')
+   
+   @include('component.sidebar')
+
+   <main>
+    @yield('')
+   </main>
+    
+</body>
+</html>
